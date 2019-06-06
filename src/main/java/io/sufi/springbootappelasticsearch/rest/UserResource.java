8fc3f1dff508924c9296385abad0058e3d30f764
Sufi Nawaz <sufinawaz@gmail.com>
2019-06-06 14:46:34 -0400

    fixed a bug for path params

--- src/main/java/io/sufi/springbootappelasticsearch/rest/UserResource.java
+++ src/main/java/io/sufi/springbootappelasticsearch/rest/UserResource.java
@@ -3,13 +3,13 @@ package io.sufi.springbootappelasticsearch.rest;
 import io.sufi.springbootappelasticsearch.model.User;
 import io.sufi.springbootappelasticsearch.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -29,7 +29,7 @@ public class UserResource {
 
     @RequestMapping("/user/username/{username}")
     @ResponseBody
-    List<User> getUserByUsername(@PathParam("username") final String username) {
+    List<User> getUserByUsername(@PathVariable("username") final String username) {
         return userService.getUserByUsername(username);
     }
 }
